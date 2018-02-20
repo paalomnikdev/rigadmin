df6d019428013698e0e98699d34580fca06386ee
Ivan Cherniy <paalomnik@gmail.com>
2018-02-20 22:36:53 +0200

    chart added

--- resources/views/card-grid.blade.php
+++ resources/views/card-grid.blade.php
@@ -80,9 +80,99 @@
         </tbody>
     </table>
 </div>
+<div id="chartContainer"></div>
 <script type="text/javascript" src="{!! asset('js/rig-control.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('js/canvasjs.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('js/jquery.canvasjs.min.js') !!}"></script>
 <script>
     jQuery(document).on('ready pjax:end', function () {
         RigControl.init({{ $rig->id }});
     });
+
+    window.onload = function () {
+
+        var options = {
+            animationEnabled: true,
+            theme: "light2",
+            title:{
+                text: "Temperature statistics"
+            },
+            axisX:{
+                valueFormatString: "DD MMM hh:mm TT"
+            },
+            axisY: {
+                title: "Temperature"
+            },
+            toolTip:{
+                shared:true
+            },
+            legend:{
+                cursor:"pointer",
+                verticalAlign: "bottom",
+                horizontalAlign: "left",
+                dockInsidePlotArea: true,
+                itemclick: toogleDataSeries
+            },
+            data: [{
+                type: "line",
+                showInLegend: true,
+                name: "Lowest temperature",
+                xValueFormatString: "DD MMM hh:mm TT",
+                color: "blue",
+                dataPoints: [
+                    { x: new Date(2017, 10, 1, 23, 12), y: 63 },
+                    { x: new Date(2017, 10, 2, 23, 13), y: 69 },
+                    { x: new Date(2017, 10, 3, 23, 14), y: 65 },
+                    { x: new Date(2017, 10, 4), y: 70 },
+                    { x: new Date(2017, 10, 5), y: 71 },
+                    { x: new Date(2017, 10, 6), y: 65 },
+                    { x: new Date(2017, 10, 7), y: 73 },
+                    { x: new Date(2017, 10, 8), y: 96 },
+                    { x: new Date(2017, 10, 9), y: 84 },
+                    { x: new Date(2017, 10, 10), y: 85 },
+                    { x: new Date(2017, 10, 11), y: 86 },
+                    { x: new Date(2017, 10, 12), y: 94 },
+                    { x: new Date(2017, 10, 13), y: 97 },
+                    { x: new Date(2017, 10, 14), y: 86 },
+                    { x: new Date(2017, 10, 15), y: 89 }
+                ]
+            },
+                {
+                    type: "line",
+                    showInLegend: true,
+                    name: "Highest Temperature",
+                    lineDashType: "dash",
+                    color: 'red',
+                    yValueFormatString: "#,##0K",
+                    dataPoints: [
+                        { x: new Date(2017, 10, 1), y: 60 },
+                        { x: new Date(2017, 10, 2), y: 57 },
+                        { x: new Date(2017, 10, 3), y: 51 },
+                        { x: new Date(2017, 10, 4), y: 56 },
+                        { x: new Date(2017, 10, 5), y: 54 },
+                        { x: new Date(2017, 10, 6), y: 55 },
+                        { x: new Date(2017, 10, 7), y: 54 },
+                        { x: new Date(2017, 10, 8), y: 69 },
+                        { x: new Date(2017, 10, 9), y: 65 },
+                        { x: new Date(2017, 10, 10), y: 66 },
+                        { x: new Date(2017, 10, 11), y: 63 },
+                        { x: new Date(2017, 10, 12), y: 67 },
+                        { x: new Date(2017, 10, 13), y: 66 },
+                        { x: new Date(2017, 10, 14), y: 56 },
+                        { x: new Date(2017, 10, 15), y: 64 }
+                    ]
+                }]
+        };
+        $("#chartContainer").CanvasJSChart(options);
+
+        function toogleDataSeries(e){
+            if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+            } else{
+                e.dataSeries.visible = true;
+            }
+            e.chart.render();
+        }
+
+    }
 </script>
