13645c2322f14927c8319dfe1ab4387b4bf0f92c
Ivan Cherniy <paalomnik@gmail.com>
2018-02-24 13:45:06 +0200

    button class replaced

--- resources/views/card-grid.blade.php
+++ resources/views/card-grid.blade.php
@@ -12,14 +12,14 @@
             data-placement="top"
             title="Reboot rig"
             type="button"
-            class="btn btn-danger fa fa-undo"></button>
+            class="reboot btn btn-danger fa fa-undo"></button>
     <button
             data-rigId="{{ $rig->id }}"
             data-toggle="tooltip"
             data-placement="top"
             title="Power off rig"
             type="button"
-            class="reboot btn btn-danger fa fa-power-off"></button>
+            class="btn btn-danger fa fa-power-off"></button>
     <table class="table">
         <thead>
             <tr>
